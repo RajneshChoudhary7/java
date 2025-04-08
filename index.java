42d54de1c9a42f74a3372c18e8700ff6369d8825
RajneshChoudhary <ahirwarrajnesh@gmail.com>
2025-04-08 13:57:44 +0530

    my second commit

--- index.java
+++ index.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello my name is rajnesh ");
+        System.out.println("Hello my name is rajnesh king ");
     }
     
 }
