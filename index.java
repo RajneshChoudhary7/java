4212dc44e3f5b0bd2c0d48fe8256de17ae1fe0e8
RajneshChoudhary <ahirwarrajnesh@gmail.com>
2025-04-08 12:51:37 +0530

    my first code

---
+++ index.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello my name is rajnesh ");
+    }
+    
+}
