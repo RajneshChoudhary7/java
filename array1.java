cd94066c30ae5e562ae4616d00a204ff088c2468
RajneshChoudhary <ahirwarrajnesh@gmail.com>
2025-04-17 16:03:48 +0530

    array questions

---
+++ array1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the element of array :-");
+        int arr[] = new int[5];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = (int) sc.nextInt();
+        }
+        System.out.println("array element is = ");
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
