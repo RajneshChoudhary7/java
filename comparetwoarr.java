60e2d2091ad427b0c4f13330c33e3b1240674505
RajneshChoudhary <ahirwarrajnesh@gmail.com>
2025-04-22 16:03:33 +0530

    stringbasic,and 2d array

---
+++ comparetwoarr.java
@@ -0,0 +1,20 @@
+public class comparetwoarr {
+    public static void main(String[] args) {
+        
+        int [] arr ={1,2,3,4};
+        int [] arr1 ={2,3,4,5};
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr1.length;j++)
+            {
+                if(arr[i]==arr1[j])
+                {
+                    System.out.println("The common element is "+arr[i]);
+                }
+            }
+        }
+        System.out.println("The common element is not found ");
+    }
+    
+}
