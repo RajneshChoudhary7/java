4c4a8223abebc33299b5c8d0883acb6920569e26
Rajneshking <ahirwarrajnesh493@gmail.com>
2025-06-04 12:48:47 +0530

    runnabe methid

---
+++ .vscode/multithreading/runnable.java
@@ -0,0 +1,21 @@
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("hiiiii");
+    }
+}
+
+
+public class runnable {
+    public static void main(String[] args) {
+        MyRunnable obj  = new MyRunnable();
+            Thread t1 = new Thread(obj);
+            t1.start();
+            // try {
+            //     t1.join();
+            // } catch (InterruptedException e) {
+            //     e.printStackTrace();
+            // }
+
+            System.out.println("rajnesh in main");
+    }
+}
