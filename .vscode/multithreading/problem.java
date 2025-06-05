3f03f18f561fb49c39faf2e8b21e47468aade5b1
Rajneshking <ahirwarrajnesh493@gmail.com>
2025-06-05 14:06:38 +0530

    multithreading ke aange ka problem

--- .vscode/multithreading/problem.java
+++ .vscode/multithreading/problem.java
@@ -1,46 +1,35 @@
 class WithdrawThread extends Thread {
-    static int totalBalance=1000;
+    static int totalBalance = 1000;
     int amount;
-    public WithdrawThread(int amount)
-    {
-        this.amount=amount;
+
+    public WithdrawThread(int amount) {
+        this.amount = amount;
 
     }
 
-    public void run(){
-        if(totalBalance>=amount)
-        {
-            System.out.println("le re l##d ke = "+amount);
-            try{
-            Thread.sleep(1000);
-            }
-            catch(InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-            totalBalance-=amount;
-            System.out.println("your remaining balance "+totalBalance);
+    public void run() {
+        synchronized (WithdrawThread.class) {
+            if (totalBalance >= amount) {
+                System.out.println("le re l##d ke = " + amount);
+                // try {
+                //     Thread.sleep(1000);
+                // } catch (InterruptedException e) {
+                //     e.printStackTrace();
+                // }
+                totalBalance -= amount;
+                System.out.println("your remaining balance " + totalBalance);
 
-        }
-        else{
-            System.out.println(" ye gareeb es ki ma...#cu...");
+            } else {
+                System.out.println(" ye gareeb es ki ma...#cu...");
+            }
         }
     }
-}
-
-
 
 public class problem {
     public static void main(String[] args) {
     WithdrawThread t1 = new WithdrawThread(800);
      WithdrawThread t2 = new WithdrawThread(500);
      t1.start();
-        try {
-            t1.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
      t2.start();
     }
-}
+}}
